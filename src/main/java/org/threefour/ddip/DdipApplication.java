15d4be045104a0fa7b10df604baaaf2f7d62d248
hellmir <hellmir610@gmail.com>
2024-09-11 15:12:56 +0900

    Initial commit (#1)
    - 프로덕션 실행 클래스 추가
    - 테스트 실행 클래스 추가
    - gradle 설정 추가

---
+++ src/main/java/org/threefour/ddip/DdipApplication.java
@@ -0,0 +1,12 @@
+package org.threefour.ddip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
+public class DdipApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DdipApplication.class, args);
+    }
+}
