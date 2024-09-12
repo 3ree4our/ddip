87b7ec7f8405c5b0954959000a6a3a6c31d4beef
hellmir <hellmir610@gmail.com>
2024-09-12 09:23:23 +0900

    chore(logging): 전체 프로젝트의 로깅 프로세스 추가 (#12)
    - 로깅 설정 파일
        - logback-spring.xml
        - 애플리케이션 설정 추가
    - LoggingInterceptor
        - 전체 Controller Components의 요청과 응답 로깅
        - 시간 성능 측정
        - 메모리 성능 측정
    - LoggingAspect
        - 전체 Service Components의 작업 수행 로깅
        - 시간 성능 측정
        - 메모리 성능 측정

---
+++ src/main/java/org/threefour/ddip/configuration/InterceptorConfig.java
@@ -0,0 +1,14 @@
+package org.threefour.ddip.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.threefour.ddip.interception.LoggingInterceptor;
+
+@Configuration
+public class InterceptorConfig implements WebMvcConfigurer {
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LoggingInterceptor());
+    }
+}
