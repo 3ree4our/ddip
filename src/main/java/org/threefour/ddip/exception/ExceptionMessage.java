2fd06812af71681713f2b52d7651372cb736d924
Sung Hyobin <hellmir610@gmail.com>
2024-09-15 16:02:29 +0900

    feat(image): 이미지 저장 요청 기능 구현 (#37)
    - 이미지 저장 요청
    - 204 status code 반환

--- src/main/java/org/threefour/ddip/exception/ExceptionMessage.java
+++ src/main/java/org/threefour/ddip/exception/ExceptionMessage.java
@@ -3,4 +3,12 @@ package org.threefour.ddip.exception;
 public class ExceptionMessage {
     public static final String PARSING_LONG_EXCEPTION_MESSAGE
             = "숫자값만 Long 타입으로 변환할 수 있습니다. 현재 변환 대상 값: %s";
+    public static final String PARSING_INTEGER_EXCEPTION_MESSAGE
+            = "숫자값만 Integer 타입으로 변환할 수 있습니다. 현재 변환 대상 값: %s";
+    public static final String PARSING_SHORT_EXCEPTION_MESSAGE
+            = "숫자값만 Short 타입으로 변환할 수 있습니다. 현재 변환 대상 값: %s";
+    public static final String PARSING_FLOAT_EXCEPTION_MESSAGE
+            = "소수값만 float 타입으로 변환할 수 있습니다. 현재 변환 대상 값: %s";
+    public static final String INVALID_TARGET_TYPE_EXCEPTION_MESSAGE
+            = "존재하는 도메인만 TargetType으로 변환할 수 있습니다. 현재 변환 대상 값: %s";
 }
