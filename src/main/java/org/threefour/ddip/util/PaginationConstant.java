66723ff9f006f7cadb0bf6063bd0c22f81e30531
Sung Hyobin <hellmir610@gmail.com>
2024-09-17 23:00:33 +0900

    chore(util): 페이지네이션 상수 정보를 담고 있는 유틸리티 클래스 추가 (#83)
    - PaginationConstant

---
+++ src/main/java/org/threefour/ddip/util/PaginationConstant.java
@@ -0,0 +1,9 @@
+package org.threefour.ddip.util;
+
+public class PaginationConstant {
+    public static final String TRUE = "true";
+    public static final String MINUS_ONE = "-1";
+    public static final String ZERO = "0";
+    public static final String NINE = "9";
+    public static final String ORDER_BY_CREATED_AT_DESCENDING = "createdAt,desc";
+}
